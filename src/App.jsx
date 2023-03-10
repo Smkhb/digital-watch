48e59adc7d8f16f416801bb65123f80508917c78
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-03-10 08:06:27 -0300

    Watch

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,25 @@ function App() {
 
   return (
     <div>
+      <main className='bg-gradient-to-t from-cyan-300 to-sky-700'>
 
+        <section className='min-h-screen flex justify-center items-center gap-3'>
+
+          <div className='text-8xl font-medium bg-blue-400 rounded-lg p-6 shadow-xl shadow-sky-800/40 '>
+            <h2 className=' text-gray-800'>12</h2>
+          </div>
+
+          <div className='text-8xl font-medium bg-blue-400 rounded-lg p-6 shadow-xl shadow-sky-800/40 '>
+            <h2 className=' text-gray-800'>46</h2>
+          </div>
+
+          <div className='text-8xl font-medium bg-blue-400 rounded-lg p-6 shadow-xl shadow-sky-800/40 '>
+            <h2 className=' text-gray-800'>01</h2>
+          </div>
+
+        </section>
+
+      </main>
     </div>
   )
 }
